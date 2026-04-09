4813c9d8f240fa845098be0158396db7159f5d3c
Whilfred <fathanemarcos169@gmail.com>
2026-04-09 14:39:50 +0000

    update

--- front_end/assets/js/dashboard.js
+++ front_end/assets/js/dashboard.js
@@ -1,7 +1,7 @@
 // ══════════════════════════════════════
 //  CONFIG
 // ══════════════════════════════════════
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://eseafy-backend.fly.dev/api';
 
 // ══════════════════════════════════════
 //  RÉCUPÉRER LE TOKEN ET L'UTILISATEUR
