c8e28d8e4b229d60c0a7eac2cd2d771eb497756b
Whilfred <fathanemarcos169@gmail.com>
2026-04-15 15:53:05 +0000

    admin tets

--- eseafy-admin/config/db.js
+++ eseafy-admin/config/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 5432,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+  ssl: process.env.DB_SSL === 'false' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
